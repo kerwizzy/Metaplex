6a48245639e0e4fce5e33837715b96ba4c2b91d0
kerwizzy <kerwizzy@gmail.com>
2017-12-26 17:55:36 -0500

    Added a data parameter to all recursive calls, allowing transfer of data (such as the value of $fn) across layers. Doing this allowed working around and OpenSCAD limitation about $fn.

--- exporters/scad.js
+++ exporters/scad.js
@@ -7,104 +7,104 @@ var headers = []
 
 var operators = {
 	"translate":{
-		parse(o,l) {
-			return "translate(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l)
+		parse(o,l,d) {
+			return "translate(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l,d)
 		}	
 	}
 	,"rotate":{
-		parse(o,l) {
+		parse(o,l,d) {
 			if (o.x == 0 && o.y == 0) {
-				return "rotate("+o.z+") "+parse(o,l)
+				return "rotate("+o.z+") "+parse(o,l,d)
 			} else {
-				return "rotate(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l)
+				return "rotate(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l,d)
 			}
 		}		
 	}
 	,"scale":{
-		parse(o,l) {
-			return "scale(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l)
+		parse(o,l,d) {
+			return "scale(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l,d)
 		}		
 	}
 	,"mirror":{
-		parse(o,l) {
-			return "mirror(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l)
+		parse(o,l,d) {
+			return "mirror(["+o.x+","+o.y+","+o.z+"]) "+parse(o,l,d)
 		}		
 	}
 	,"linear_extrude":{
-		parse(o,l) {
-			return "linear_extrude(height="+o.height+",convexity=10,twist="+o.twist+",scale="+o.scale+") {\n"+parse(o,l)+"\n}"
+		parse(o,l,d) {
+			return "linear_extrude(height="+o.height+",convexity=10,twist="+o.twist+",scale="+o.scale+") {\n"+parse(o,l,d)+"\n}"
 		}
 	}
 	,"rotate_extrude":{
-		parse(o,l) {
-			return "rotate_extrude(angle="+o.angle+",convexity=2) {\n"+parse(o,l)+"\n}"
+		parse(o,l,d) {
+			return "rotate_extrude(angle="+o.angle+",convexity=2) {\n"+parse(o,l,d)+"\n}"
 		}
 	}
 	,"cube":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return "cube("+o.size+")"
 		}		
 	}
 	,"cylinder":{
-		parse(o,l) {
+		parse(o,l,d) {
 			if (typeof o.radius == "undefined") {
 				err("Radius is undefined. "+l.errtext)
 			}
-			return "cylinder(h="+o.height+",r="+o.radius+")"
+			return "cylinder(h="+o.height+",r="+o.radius+genFnParam(d)+")"
 		}
 	}
 	,"sphere":{
-		parse(o,l) {
-			return "sphere("+o.radius+")"
+		parse(o,l,d) {
+			return "sphere("+o.radius+genFnParam(d)+")"
 		}
 	}
 	,"box":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return "cube(["+o.width+","+o.depth+","+o.height+"])"
 		}
 	}
 	,"cone":{
-		parse(o,l) {
-			return "cylinder(h="+o.height+",r1="+o.radius1+",r2="+o.radius2+")"
+		parse(o,l,d) {
+			return "cylinder(h="+o.height+",r1="+o.radius1+",r2="+o.radius2+genFnParam(d)+")"
 		}
 	}
 	,"rectangle":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return "square(["+o.x+","+o.y+"])"
 		}
 	}
 	,"circle":{
-		parse(o,l) {
-			return "circle("+o.radius+")"
+		parse(o,l,d) {
+			return "circle("+o.radius+genFnParam(d)+")"
 		}
 	}
 	,"polygon":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return "polygon("+JSON.stringify(o.points)+")"
 		}
 	}
 	,"text":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return `text("${o.text}",${o.size},"${o.font}",halign="${o.halign}",valign="${o.valign}",spacing=${o.spacing},direction="${o.direction}",language="${o.language}",script="${o.script}")`
 		}	
 	}
 	,"union":{
-		parse(o,l) {
-			return "union() {\n"+parse(o,l,"child1")+"\n"+parse(o,l,"child2")+"\n}"
+		parse(o,l,d) {
+			return "union() {\n"+parse(o,l,"child1",d)+"\n"+parse(o,l,"child2",d)+"\n}"
 		}
 	}
 	,"difference":{
-		parse(o,l) {
-			return "difference() {\n"+parse(o,l,"child1")+"\n"+parse(o,l,"child2")+"\n}"
+		parse(o,l,d) {
+			return "difference() {\n"+parse(o,l,"child1",d)+"\n"+parse(o,l,"child2",d)+"\n}"
 		}
 	}
 	,"intersection":{
-		parse(o,l) {
-			return "intersection() {\n"+parse(o,l,"child1")+"\n"+parse(o,l,"child2")+"\n}"
+		parse(o,l,d) {
+			return "intersection() {\n"+parse(o,l,"child1",d)+"\n"+parse(o,l,"child2",d)+"\n}"
 		}
 	}
 	,"offset":{
-		parse(o,l) {
+		parse(o,l,d) {
 			var out;
 			if (o.mode == "radius") {
 				out = "offset(r="+o.distance+")"
@@ -115,33 +115,47 @@ var operators = {
 			} else {
 				err("Offset mode "+o.mode+" is not supported.")
 			}
-			return out+" "+parse(o,l)
+			return out+" "+parse(o,l,d)
 		}
 	}
 	,"$fn":{
-		parse(o,l) {
-			return "{\n$fn="+o.fn+"\n"+parse(o,l)+"\n}"
+		parse(o,l,d) {
+			var dataCopy = copy(d)
+			dataCopy.fn = o.fn
+			return "//$fn="+o.fn+"\n"+parse(o,l,dataCopy)
 		}
 	}
 	,"colorname":{
-		parse(o,l) {
-			return 'color("'+o.name+'",'+o.alpha+') '+parse(o,l)
+		parse(o,l,d) {
+			return 'color("'+o.name+'",'+o.alpha+') '+parse(o,l,d)
 		}
 	}
 	,"colorrgba":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return "color(["+o.r/255+","+o.g/255+","+o.b/255+","+o.a/255+"])"
 		}
 	}
 	,"import":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return 'import("'+o.path+'",convexity='+10+')'
 		}
 	}
 	,"empty":{
-		parse(o,l) {
+		parse(o,l,d) {
 			return ""
 		}
+	}
+}
+
+function copy(obj) {
+	return JSON.parse(JSON.stringify(obj))
+}
+
+function genFnParam(d) {
+	if (d.fn != defaultData.fn) {
+		return ",$fn="+d.fn
+	} else {
+		return ""
 	}
 }
 
@@ -168,8 +182,18 @@ class Location {
 	}
 }
 
-function parse(obj,loc,tag) {
-	if (!loc) {
+var defaultData = {
+	fn:0
+}
+
+function parse(obj,loc,tag,data) {
+	//{... child:{}},Location {},{fn:90} -> {... child:{}},Location {},undefined,{fn:90}
+	if (typeof data == "undefined") {
+		data = tag
+		tag = undefined
+	}
+	
+	if (!loc) { //usually only true if root
 		loc = new Location()
 	} else {
 		if (!tag) {
@@ -181,7 +205,7 @@ function parse(obj,loc,tag) {
 	var type = obj.type
 	if (operators[type]) {
 		try {
-			return operators[type].parse(obj,loc)
+			return operators[type].parse(obj,loc,data)
 		} catch(error) {
 			err(error+", loc = "+loc.errtext)
 		}
@@ -214,7 +238,7 @@ const DEFAULT_OPTIONS = {
 function initparse(data,options) {
 	options = combineOptionsListWithDefaults(options,DEFAULT_OPTIONS)
 	debug = options.debug
-	var out = parse(data)
+	var out = parse(data,undefined,defaultData)
 	
 	//Add semicolons.
 	out = out.split("\n");
